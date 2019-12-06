7818591fd78463565444e88b3e6e102c146db7dc
Muriel <matheus.muriel@outlook.com>
2019-12-06 15:06:20 -0300

    Metatags e title

--- aaaa.rb
+++ aaaa.rb
@@ -1,5 +1,6 @@
 require 'httparty'
 require 'zip'
+require "ogpr"
 
 regex_url = /^(http(s)?:\/\/www\.hackerrank\.com\/challenges\/){1}([a-zA-Z]|[0-9])+(\-([a-zA-Z]|[0-9])+)*\/(problem$)?/
 
@@ -17,13 +18,27 @@ end
 url = url.gsub(/\s|\n/, '').gsub(/\/problem$|\/problem\/$/, '')
 
 # Download test cases
-url_testecase = url.gsub(/\/challenges\//, '/rest/contests/master/challenges/') + "/download_testcases"
+def get_testcase
+    url_testecase = url.gsub(/\/challenges\//, '/rest/contests/master/challenges/') + "/download_testcases"
 
-zip_file = HTTParty.get(url_testecase).body
-Zip::InputStream.open(StringIO.new(zip_file)) do |io|
-    while entry = io.get_next_entry
-      file_path = File.join(Dir.pwd, entry.name)
-      FileUtils.mkdir_p(File.dirname(file_path))
-      entry.extract(file_path) unless File.exist?(file_path)
+    zip_file = HTTParty.get(url_testecase).body
+    Zip::InputStream.open(StringIO.new(zip_file)) do |io|
+        while entry = io.get_next_entry
+        file_path = File.join(Dir.pwd, entry.name)
+        FileUtils.mkdir_p(File.dirname(file_path))
+        entry.extract(file_path) unless File.exist?(file_path)
+        end
     end
-  end
+end
+
+def get_informations(url)
+    # Busca e faz um parser nas matatags da url
+    ogp = Ogpr.fetch(url)
+
+    # Abre a metatag OpenGraph
+    #og = ogp.open_graph
+    titulo = ogp.title
+    p titulo
+end
+
+get_informations(url)
